0fb5e7408bf1208d7081dab44eb0f89bc3d57b8e
Manan dudeja <manandudeja6@gmail.com>
2024-05-19 21:23:59 +0530

    analytics changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from './Components/Home';
-import Navbar from './Components/Navbar';
-import About from "./Components/About"
-import Services from './Components/Services';
-import Portfolio from './Components/Portfolio';
-import Prices from './Components/Prices';
-import Contact from './Components/Contact';
-import Footer from './Components/Footer';
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./Components/Home";
+import Navbar from "./Components/Navbar";
+import About from "./Components/About";
+import Services from "./Components/Services";
+import Portfolio from "./Components/Portfolio";
+import Prices from "./Components/Prices";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 function App() {
   return (
-    <div className=' overflow-x-hidden '>
-      {/* <Navbar/> */}
+    <div className=" overflow-x-hidden ">
+      <Analytics id="G-1ZQZQZQZQZ" />
       <Home />
       <About />
       <Services />
