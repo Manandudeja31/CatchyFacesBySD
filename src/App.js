2025c9275c06647320581e2e093f64aa3c266c1d
Manan dudeja <manandudeja6@gmail.com>
2024-05-19 21:35:36 +0530

    speed insights

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Home from "./Components/Home";
-import Navbar from "./Components/Navbar";
 import About from "./Components/About";
 import Services from "./Components/Services";
 import Portfolio from "./Components/Portfolio";
@@ -9,10 +7,12 @@ import Prices from "./Components/Prices";
 import Contact from "./Components/Contact";
 import Footer from "./Components/Footer";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 function App() {
   return (
     <div className=" overflow-x-hidden ">
       <Analytics id="G-1ZQZQZQZQZ" />
+      <SpeedInsights id="G-1ZQZQZQZQZ" />
       <Home />
       <About />
       <Services />
